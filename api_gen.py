4059407bc1a9fa411cb4cc38594731e209d45e26
dmorton714 <102087408+dmorton714@users.noreply.github.com>
2023-11-06 12:39:58 -0500

    added a few projects

--- api_gen.py
+++ api_gen.py
@@ -1,29 +1,35 @@
-# url = 'place URL here'
+import pandas as pd 
+import json
+from arcgis.features import FeatureLayer
+import requests
+import time
 
-# batch_size = 1000  # Number of records to retrieve per batch
-# offset = 0  # Initial offset value
-# data_list = []
-# # ​Create the feature layer object
-# feature_layer = FeatureLayer(url)
+url = 'place URL here'
 
-# while True:
-#     # Query the feature layer with pagination
-#     query_result = feature_layer.query(where='1=1', out_fields='*', return_geometry=False, result_offset=offset, result_record_count=batch_size)
-    
-#     # Retrieve the features from the query result
-#     features = query_result.features
-    
-#     # Process the data for the current batch
-#     for feature in features:
-#         data_list.append(feature.attributes)
-    
-#     # Break the loop if the response is empty or the desired number of records is reached
-#     if len(features) == 0 or len(data_list) >= 1000:
-#         break
-    
-#     # Increment the offset by the batch size
-#     offset += batch_size
-# # Create a DataFrame from the data list
-# df = pd.DataFrame(data_list)
+batch_size = 1000  # Number of records to retrieve per batch
+offset = 0  # Initial offset value
+data_list = []
+# ​Create the feature layer object
+feature_layer = FeatureLayer(url)
 
-# df.to_csv('../json_files/local_working.csv')
+while True:
+    # Query the feature layer with pagination
+    query_result = feature_layer.query(where='1=1', out_fields='*', return_geometry=False, result_offset=offset, result_record_count=batch_size)
+    
+    # Retrieve the features from the query result
+    features = query_result.features
+    
+    # Process the data for the current batch
+    for feature in features:
+        data_list.append(feature.attributes)
+    
+    # Break the loop if the response is empty or the desired number of records is reached
+    if len(features) == 0 or len(data_list) >= 1000:
+        break
+    
+    # Increment the offset by the batch size
+    offset += batch_size
+# Create a DataFrame from the data list
+df = pd.DataFrame(data_list)
+
+df.to_csv('local_working.csv')
